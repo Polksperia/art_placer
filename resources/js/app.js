49aa1a3298433ba45553cf0ba5507ef949af3254
Polksperia <rheinoheffley@gmail.com>
2023-11-14 12:46:20 +0700

    Today's edit
    Added google maps api, adding carousel, fixing navbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
 import './bootstrap';
+
+$(document).ready(function(){
+    $('.carousel').carousel();
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,18 @@
 
 <!-- Content of the page -->
 @section('content')
-    <div class = "imgbox">
-        <img class = "center-fit" src="images\Benczr-Gyula-The-Recapture-of-Buda-Castle-in-1686.jpg">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="images\Gyula-Benczúr-The-Recapture-of-Buda-Castle-in-1686-1896.jpg?auto=yes" class="d-block w-100" alt="First Slide">
+            </div>
+            <div class="carousel-item">
+                <img src="images\Claude-Monet-Effect-of-Spring-Giverny-1890.jpg?auto=yes" class="d-block w-100" alt="Second Slide">
+            </div>
+            <div class="carousel-item">
+                <img src="images\Katsushika-Hokusai-The-Great-Wave-off-Kanagawa-1831.jpg?auto=yes" class="d-block w-100" alt="Third Slide">
+            </div>
+        </div>
     </div>
     <!-- This is section for homepage -->
 @endsection
