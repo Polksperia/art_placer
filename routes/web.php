cfcc8b527f171cb6feea4fdb025aaff19a95942b
Polksperia <rheinoheffley@gmail.com>
2023-11-21 11:57:37 +0700

    Changed to use controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/login', function () {
-    return view('login');
-});
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/login', [LoginController::class, 'showLoginForm']);
